4b4f0a06d8f5ff6a7d5b74c5ec3f7ac7c1a6c68d
heejin342 <tktmzp0526@naver.com>
2022-04-16 18:40:36 +0900

    add coreData, notification center

--- NetflexClone/Views/CollectionviewTableViewCell.swift
+++ NetflexClone/Views/CollectionviewTableViewCell.swift
@@ -56,17 +56,14 @@ class CollectionViewTableViewCell: UITableViewCell {
     
     private func downloadTitleAt(indexPath: IndexPath) {
         
-    
-//        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
-//            switch result {
-//            case .success():
-//                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//            }
-//        }
-//        
-
+        DataPersistanceMaganer.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+               NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
